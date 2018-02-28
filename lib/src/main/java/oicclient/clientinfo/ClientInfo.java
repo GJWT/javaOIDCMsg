f931b27bcda10a03b10bbdfd9b1df46b55dea54a
Justin Dahmubed <jdahmubed@google.com>
2018-02-28 11:44:31 -0800

    add import

--- lib/src/main/java/oicclient/clientinfo/ClientInfo.java
+++ lib/src/main/java/oicclient/clientinfo/ClientInfo.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import oicclient.exceptions.ValueError;
 import org.slf4j.LoggerFactory;
+import oicclient.state.State;
 
 public class ClientInfo {
 
